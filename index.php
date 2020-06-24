cf0f678a7008249c596141543a346cdaacab6a75
Shion <a951753abc@gmail.com>
2020-06-24 22:34:28 +0800

    Modify 換成裏版

--- index.php
+++ index.php
@@ -6,55 +6,73 @@
     <meta name="description" content="">
     <meta name="author" content="">
     <link rel="icon" href="favicon.ico">
-    <title>探偵のなく頃に</title>
+    <title>探偵のなく頃に‧裏</title>
     <!-- Bootstrap core CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
           integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <!-- Custom styles for this template -->
     <link href="css/simple-sidebar.css" rel="stylesheet">
 </head>
-
 <body>
-
-<div class="d-flex" id="wrapper">
-
-    <!-- Sidebar -->
-    <div class="bg-light border-right" id="sidebar-wrapper">
-        <div class="sidebar-heading">探偵のなく頃に</div>
-        <div class="list-group list-group-flush">
-            <a href="#" class="list-group-item list-group-item-action bg-light">首頁</a>
-            <a href="#" class="list-group-item list-group-item-action bg-light">Shortcuts</a>
-            <a href="#" class="list-group-item list-group-item-action bg-light">Overview</a>
-            <a href="#" class="list-group-item list-group-item-action bg-light">Events</a>
-            <a href="#" class="list-group-item list-group-item-action bg-light">Profile</a>
-            <a href="#" class="list-group-item list-group-item-action bg-light">Status</a>
-        </div>
-    </div>
-    <!-- /#sidebar-wrapper -->
-
+<div class="d-flex" id="wrapper">​
     <!-- Page Content -->
     <div id="page-content-wrapper">
-
-        <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
-            <button class="btn btn-dark" id="menu-toggle">選單</button>
-
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-                    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-        </nav>
-
         <div class="container-fluid">
-            <h1 class="mt-4">探偵のなく頃に</h1>
-            <div class="card border-danger mb-3" style="max-width: 18rem;">
-                <div class="card-body text-danger">
-                    <p class="card-text">這是被確立的事實</p>
+            <div class="row">
+                <div class="col-4">
+                    <div class="card border-primary   mb-3" >
+                        <div class="card-header">盤面法則</div>
+                        <div class="card-body text-primary  ">
+                            <p class="card-text">推理魔女在配電室被燒成焦屍</p>
+                        </div>
+                    </div>
+                    <div class="card border-primary   mb-3" >
+                        <div class="card-header">調書1</div>
+                        <div class="card-body text-primary  ">
+                            <p class="card-text">魔力:1<br>
+                                告發條件：無 <br>
+                                必要證言數量：0<br>
+                                殺害宴會主人的人是「          」</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-4">
+                    <div class="card border-danger mb-3" >
+                        <div class="card-header">真實-1</div>
+                        <div class="card-body text-danger">
+                            <p class="card-text">推理魔女在配電室被燒成焦屍</p>
+                        </div>
+
+                    </div>
+                    <div class="card border-danger mb-3" >
+                        <div class="card-header">真實-2</div>
+                        <div class="card-body text-danger">
+                            <p class="card-text">推理魔女在配電室被燒成焦屍</p>
+                        </div>
+
+                    </div>
+                </div>
+                <div class="col-4">
+                    <div class="card border-info mb-3">
+                        <div class="card-header">幻想-1</div>
+                        <div class="card-body text-info">
+                            <p class="card-info">
+                                (8)
+                                最後手段 指控 正義之友 透過「暴力手段」「棄子」「演戲」「不善推理」「打工仔」「只推理警方案件」「跟其他偵探無合作」「擅長看資料」
+                        </div>
+                    </div>
+                    <div class="card border-info mb-3">
+                        <div class="card-header">幻想-2</div>
+                        <div class="card-body text-info">
+                            <p class="card-info">
+                                [祕密]20點
+                            </p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-    <!-- /#page-content-wrapper -->
-
 </div>
 <!-- Optional JavaScript -->
 <!-- jQuery first, then Popper.js, then Bootstrap JS -->
@@ -75,6 +93,5 @@
         $("#wrapper").toggleClass("toggled");
     });
 </script>
-
 </body>
 </html>
