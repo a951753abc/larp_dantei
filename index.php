89f51cfae9a5a46cec697a5f0e0ccb5352775f53
Shion <a951753abc@gmail.com>
2020-06-24 23:46:03 +0800

    Add 初期配置

--- index.php
+++ index.php
@@ -18,16 +18,52 @@
     <!-- Page Content -->
     <div id="page-content-wrapper">
         <div class="container-fluid">
+            <h1 class="mt-4"><a href="dead.png" target="_blank">現場狀況圖</a></h1>
             <div class="row">
-                <div class="col-4">
+                <div class="col-2">
+                    <div class="card border-success   mb-3" >
+                        <div class="card-header">陣營1</div>
+                        <div class="card-body text-success  ">
+                            <p class="card-text">
+                                法典魔術師 <BR>
+                                赤魔術師 <BR>
+                                搖籃指引(Paradise Regained) <BR>
+                                心燈魔術師 <BR>
+                                優冥魔術師 <BR>
+                                貪婪魔術師 <BR>
+                                除錯魔術師
+                            </p>
+                        </div>
+                    </div>
+                    <div class="card border-success   mb-3" >
+                        <div class="card-header">陣營2</div>
+                        <div class="card-body text-success  ">
+                            <p class="card-text">
+                                山姆‧莫甘 <BR>
+                                玉置 瑩
+                            </p>
+                        </div>
+                    </div>
+                    <div class="card border-success   mb-3" >
+                        <div class="card-header">陣營3</div>
+                        <div class="card-body text-success  ">
+                            <p class="card-text">
+                                田中太郎
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-2">
                     <div class="card border-primary   mb-3" >
                         <div class="card-header">盤面法則</div>
                         <div class="card-body text-primary  ">
-                            <p class="card-text">推理魔女在配電室被燒成焦屍</p>
+                            <p class="card-text">
+                                所有物件的智商都在貓狗以下
+                            </p>
                         </div>
                     </div>
                     <div class="card border-primary   mb-3" >
-                        <div class="card-header">調書1</div>
+                        <div class="card-header">調書一</div>
                         <div class="card-body text-primary  ">
                             <p class="card-text">魔力:1<br>
                                 告發條件：無 <br>
@@ -35,19 +71,63 @@
                                 殺害宴會主人的人是「          」</p>
                         </div>
                     </div>
+                    <div class="card border-primary   mb-3" >
+                        <div class="card-header">調書二</div>
+                        <div class="card-body text-primary  ">
+                            <p class="card-text">
+                                魔力:2<br>
+                                告發條件：解決調書1 <br>
+                                必要證言數量：1<br>
+                                密室的手法是「          」</p>
+                        </div>
+                    </div>
+                    <div class="card border-primary   mb-3" >
+                        <div class="card-header">調書三</div>
+                        <div class="card-body text-primary  ">
+                            <p class="card-text">
+                                魔力:3<br>
+                                告發條件：解決調書1 <br>
+                                必要證言數量：2<br>
+                                屍體毀容的手法是「          」，目的是「     」</p>
+                        </div>
+                    </div>
+                    <div class="card border-primary   mb-3" >
+                        <div class="card-header">調書四</div>
+                        <div class="card-body text-primary  ">
+                            <p class="card-text">
+                                魔力:1<br>
+                                告發條件：解決調書1 <br>
+                                必要證言數量：1<br>
+                                兇手的動機是「          」</p>
+                        </div>
+                    </div>
+                    <div class="card border-primary   mb-3" >
+                        <div class="card-header">調書五</div>
+                        <div class="card-body text-primary  ">
+                            <p class="card-text">
+                                魔力:1<br>
+                                告發條件：無 <br>
+                                必要證言數量：1<br>
+                                宴會主人宣布「在我們之中，有人不是偵探。」的用意是「    」</p>
+                        </div>
+                    </div>
+                    <div class="card border-primary   mb-3" >
+                        <div class="card-header">調書六</div>
+                        <div class="card-body text-primary  ">
+                            <p class="card-text">
+                                魔力:3<br>
+                                告發條件：解決調書1~5 <br>
+                                必要證言數量：3<br>
+                                事件會由「     」解決。
+                                而後「    」 會 「    」。</p>
+                        </div>
+                    </div>
                 </div>
                 <div class="col-4">
                     <div class="card border-danger mb-3" >
                         <div class="card-header">真實-1</div>
                         <div class="card-body text-danger">
-                            <p class="card-text">推理魔女在配電室被燒成焦屍</p>
-                        </div>
-
-                    </div>
-                    <div class="card border-danger mb-3" >
-                        <div class="card-header">真實-2</div>
-                        <div class="card-body text-danger">
-                            <p class="card-text">推理魔女在配電室被燒成焦屍</p>
+                            <p class="card-text"></p>
                         </div>
 
                     </div>
@@ -56,17 +136,7 @@
                     <div class="card border-info mb-3">
                         <div class="card-header">幻想-1</div>
                         <div class="card-body text-info">
-                            <p class="card-info">
-                                (8)
-                                最後手段 指控 正義之友 透過「暴力手段」「棄子」「演戲」「不善推理」「打工仔」「只推理警方案件」「跟其他偵探無合作」「擅長看資料」
-                        </div>
-                    </div>
-                    <div class="card border-info mb-3">
-                        <div class="card-header">幻想-2</div>
-                        <div class="card-body text-info">
-                            <p class="card-info">
-                                [祕密]20點
-                            </p>
+                            <p class="card-text"></p>
                         </div>
                     </div>
                 </div>
