9c90fa6d52c70a3db22d6affc7a8d6b1d36edd30
Shion <a951753abc@gmail.com>
2020-06-24 23:12:32 +0800

    Modify 加入魔力一覽

---
+++ mp.php
@@ -0,0 +1,237 @@
+<html lang="zh-Hant-TW">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <meta name="google-signin-client_id"
+          content="300893859102-o9l9k3fckeur84kqmmh0ud9j3ttjbmf5.apps.googleusercontent.com">
+    <title>探偵のなく頃に‧裏</title>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+TC&amp;display=swap" rel="stylesheet">
+    <script src="https://apis.google.com/js/platform.js?onload=ongoogleload" async="" defer="defer"
+            gapi_processed="true"></script>
+    <link href="css/main~c5b24172.css" rel="stylesheet">
+    <style data-styled="active" data-styled-version="5.0.1"></style>
+    <style>
+        table {
+            border: 1px solid;border-color: rgb(221, 221, 221)!important;
+        }
+        td {
+            border: 0.1px solid!important;border-color: rgb(221, 221, 221)!important;
+        }
+    </style>
+<!--  死亡顏色  var(--grey)-->
+</head>
+<body>
+<div id="root">
+    <div class="sc-fzowVh equHBw">
+        <div class="sc-fzplWN hRBsWH sc-fzoiQi ozSmQ"><h3 class="sc-fznyAO CWQMf">角色一覽</h3>
+            <table class="sc-AxjAm bcMPWx" style="min-width: 560px;" >
+                <tbody class="sc-AxirZ bJCmFu">
+                <tr class="sc-AxiKw eSbheu" bgcolor="#d3d3d3" >
+                    <th class="sc-AxhCb lnpmAI" style="width: 34px;">頭像</th>
+                    <th class="sc-AxhCb lnpmAI" style="width: 32px;">行動</th>
+                    <th class="sc-AxhCb lnpmAI">
+                        <div class="sc-fzoyTs jZUSDr">
+                            <div>暱稱</div>
+                            <div>角色（稱號）</div>
+                        </div>
+                    </th>
+                    <th class="sc-AxhCb lnpmAI">魔力</th>
+                </tr>
+                <tr class="sc-AxiKw sc-fzqARJ eLpUJW" style="color: inherit;">
+                    <td class="sc-AxhUy dRdZbR" style="padding: 0px;">
+                        <div class="sc-fzozJi gUEzCW">
+                            <picture>
+                                <img src="/gold.png" alt="">
+                            </picture>
+                        </div>
+                    </td>
+                    <td class="sc-AxhUy dRdZbR">0</td>
+                    <td class="sc-AxhUy dRdZbR">
+                        <div class="sc-fzqNqU kikdAh">
+                            <div class="sc-fzoyTs jZUSDr">
+                                <div>UD</div>
+                                <div>法典魔術師</div>
+                            </div>
+                        </div>
+                    </td>
+                    <td class="sc-AxhUy dRdZbR">0</td>
+                </tr>
+                <tr class="sc-AxiKw sc-fzqARJ eLpUJW" style="color: inherit;">
+                    <td class="sc-AxhUy dRdZbR" style="padding: 0px;">
+                        <div class="sc-fzozJi gUEzCW">
+                            <picture>
+                                <img src="/aka.png" alt="">
+                            </picture>
+                        </div>
+                    </td>
+                    <td class="sc-AxhUy dRdZbR">0</td>
+                    <td class="sc-AxhUy dRdZbR">
+                        <div class="sc-fzqNqU kikdAh">
+                            <div class="sc-fzoyTs jZUSDr">
+                                <div>空咄</div>
+                                <div>赤魔術師</div>
+                            </div>
+                        </div>
+                    </td>
+                    <td class="sc-AxhUy dRdZbR">0</td>
+                </tr>
+                <tr class="sc-AxiKw sc-fzqARJ eLpUJW" style="color: inherit;">
+                    <td class="sc-AxhUy dRdZbR" style="padding: 0px;">
+                        <div class="sc-fzozJi gUEzCW">
+                            <picture>
+                                <img src="/ao.png" alt="">
+                            </picture>
+                        </div>
+                    </td>
+                    <td class="sc-AxhUy dRdZbR">0</td>
+                    <td class="sc-AxhUy dRdZbR">
+                        <div class="sc-fzqNqU kikdAh">
+                            <div class="sc-fzoyTs jZUSDr">
+                                <div>銀狼</div>
+                                <div>心燈魔術師</div>
+                            </div>
+                        </div>
+                    </td>
+                    <td class="sc-AxhUy dRdZbR">0</td>
+                </tr>
+                <tr class="sc-AxiKw sc-fzqARJ eLpUJW" style="color: inherit;">
+                    <td class="sc-AxhUy dRdZbR" style="padding: 0px;background-color: black;">
+                        <div class="sc-fzozJi gUEzCW">
+                            <picture>
+                                <img src="/white.png" alt="">
+                            </picture>
+                        </div>
+                    </td>
+                    <td class="sc-AxhUy dRdZbR">0</td>
+                    <td class="sc-AxhUy dRdZbR">
+                        <div class="sc-fzqNqU kikdAh">
+                            <div class="sc-fzoyTs jZUSDr">
+                                <div>阿翔</div>
+                                <div>優冥魔術師</div>
+                            </div>
+                        </div>
+                    </td>
+                    <td class="sc-AxhUy dRdZbR">0</td>
+                </tr>
+                <tr class="sc-AxiKw sc-fzqARJ eLpUJW" style="color: inherit;">
+                    <td class="sc-AxhUy dRdZbR" style="padding: 0px;">
+                        <div class="sc-fzozJi gUEzCW">
+                            <picture>
+                                <img src="/greed.png" alt="">
+                            </picture>
+                        </div>
+                    </td>
+                    <td class="sc-AxhUy dRdZbR">0</td>
+                    <td class="sc-AxhUy dRdZbR">
+                        <div class="sc-fzqNqU kikdAh">
+                            <div class="sc-fzoyTs jZUSDr">
+                                <div>折衷</div>
+                                <div>貪婪魔術師</div>
+                            </div>
+                        </div>
+                    </td>
+                    <td class="sc-AxhUy dRdZbR">0</td>
+                </tr>
+                <tr class="sc-AxiKw sc-fzqARJ eLpUJW" style="color: inherit;">
+                    <td class="sc-AxhUy dRdZbR" style="padding: 0px;">
+                        <div class="sc-fzozJi gUEzCW">
+                            <picture>
+                                <img src="/debug.png" alt="">
+                            </picture>
+                        </div>
+                    </td>
+                    <td class="sc-AxhUy dRdZbR">0</td>
+                    <td class="sc-AxhUy dRdZbR">
+                        <div class="sc-fzqNqU kikdAh">
+                            <div class="sc-fzoyTs jZUSDr">
+                                <div>病蘿</div>
+                                <div>除錯魔術師</div>
+                            </div>
+                        </div>
+                    </td>
+                    <td class="sc-AxhUy dRdZbR">0</td>
+                </tr>
+                <tr class="sc-AxiKw sc-fzqARJ eLpUJW" style="color: inherit;">
+                    <td class="sc-AxhUy dRdZbR" style="padding: 0px;">
+                        <div class="sc-fzozJi gUEzCW">
+                            <picture>
+                                <img src="/black.png" alt="">
+                            </picture>
+                        </div>
+                    </td>
+                    <td class="sc-AxhUy dRdZbR">0</td>
+                    <td class="sc-AxhUy dRdZbR">
+                        <div class="sc-fzqNqU kikdAh">
+                            <div class="sc-fzoyTs jZUSDr">
+                                <div>菲斯</div>
+                                <div>搖籃指引(Paradise Regained)</div>
+                            </div>
+                        </div>
+                    </td>
+                    <td class="sc-AxhUy dRdZbR">0</td>
+                </tr>
+                <tr class="sc-AxiKw sc-fzqARJ eLpUJW" style="color: inherit;">
+                    <td class="sc-AxhUy dRdZbR" style="padding: 0px;">
+                        <div class="sc-fzozJi gUEzCW">
+                            <picture>
+                                <img src="/sam.png" alt="">
+                            </picture>
+                        </div>
+                    </td>
+                    <td class="sc-AxhUy dRdZbR">0</td>
+                    <td class="sc-AxhUy dRdZbR">
+                        <div class="sc-fzqNqU kikdAh">
+                            <div class="sc-fzoyTs jZUSDr">
+                                <div>棋男</div>
+                                <div>山姆‧莫甘</div>
+                            </div>
+                        </div>
+                    </td>
+                    <td class="sc-AxhUy dRdZbR">0</td>
+                </tr>
+
+                <tr class="sc-AxiKw sc-fzqARJ eLpUJW" style="color: inherit;">
+                    <td class="sc-AxhUy dRdZbR" style="padding: 0px;">
+                        <div class="sc-fzozJi gUEzCW">
+                            <picture>
+                                <img src="/hotaru.png" alt="">
+                            </picture>
+                        </div>
+                    </td>
+                    <td class="sc-AxhUy dRdZbR">0</td>
+                    <td class="sc-AxhUy dRdZbR">
+                        <div class="sc-fzqNqU kikdAh">
+                            <div class="sc-fzoyTs jZUSDr">
+                                <div>煮魚</div>
+                                <div>玉置 瑩</div>
+                            </div>
+                        </div>
+                    </td>
+                    <td class="sc-AxhUy dRdZbR">0</td>
+                </tr>
+                <tr class="sc-AxiKw sc-fzqARJ eLpUJW" style="color: inherit;">
+                    <td class="sc-AxhUy dRdZbR" style="padding: 0px;">
+                        <div class="sc-fzozJi gUEzCW">
+                            <picture>
+                                <img src="/tanaka.png" alt="">
+                            </picture>
+                        </div>
+                    </td>
+                    <td class="sc-AxhUy dRdZbR">0</td>
+                    <td class="sc-AxhUy dRdZbR">
+                        <div class="sc-fzqNqU kikdAh">
+                            <div class="sc-fzoyTs jZUSDr">
+                                <div>小翼</div>
+                                <div>田中太郎</div>
+                            </div>
+                        </div>
+                    </td>
+                    <td class="sc-AxhUy dRdZbR">0</td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+</body>
+</html>
