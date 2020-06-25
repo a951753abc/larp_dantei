10d59b30135a147d13dc54317e6c1dd3f951c107
Shion <a951753abc@gmail.com>
2020-06-25 14:39:44 +0800

    add

--- mp.php
+++ mp.php
@@ -209,7 +209,7 @@
                     </td>
                     <td class="sc-AxhUy dRdZbR">0</td>
                 </tr>
-                <tr class="sc-AxiKw sc-fzqARJ eLpUJW" style="color: inherit;">
+                <tr class="sc-AxiKw sc-fzqARJ eLpUJW" style="color: var(--grey)">
                     <td class="sc-AxhUy dRdZbR" style="padding: 0px;">
                         <div class="sc-fzozJi gUEzCW">
                             <picture>
